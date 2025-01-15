27f0603855e6f19836376faf48cbefb1678bebc7
Jeremy Hyrkas <jeremy.hyrkas@gmail.com>
2025-01-15 13:59:57 -0800

    adding Butterworth files

--- Source/Biquad.cpp
+++ Source/Biquad.cpp
@@ -6,6 +6,14 @@
 //
 
 #include "Biquad.hpp"
+
+Biquad::Biquad() :
+b0(1), b1(0), b2(0), a1(0), a2(0)
+{
+    b_zi[0] = 0; b_zi[1] = 0;
+    a_zi[0] = 0; a_zi[1] = 0;
+}
+
 Biquad::Biquad(float _b0, float _b1, float _b2, float _a1, float _a2) :
 b0(_b0), b1(_b1), b2(_b2), a1(_a1), a2(_a2)
 {
@@ -18,8 +26,8 @@ Biquad::~Biquad() {
 }
 
 float Biquad::processSample(float sample) {
-    float outp = b0 * sample + b1 * b_zi[0] + b2 * b_zi[1] -
-    a1 * a_zi[0] - a2 * a_zi[1];
+    float outp = b0 * sample + b1 * b_zi[0] + b2 * b_zi[1]
+        - a1 * a_zi[0] - a2 * a_zi[1];
     b_zi[1] = b_zi[0];
     b_zi[0] = sample;
     a_zi[1] = a_zi[0];
@@ -41,4 +49,22 @@ void Biquad::setParams(float _b0, float _b1, float _b2, float _a1, float _a2) {
 void Biquad::clear() {
     b_zi[0] = 0; b_zi[1] = 0;
     a_zi[0] = 0; a_zi[1] = 0;
+}
+
+// converting from df2t form to direct form 1
+void Biquad::DF2TBiquad(double B0, double B1, double B2,
+                        double A0, double A1, double A2) {
+    b0 = B0  / A0;
+    b1 = B1  / A0;
+    b2 = B2  / A0;
+    a1 = (A1) / A0;
+    a2 = (A2) / A0;
+    b_zi[0] = 0; b_zi[1] = 0;
+    a_zi[0] = 0; a_zi[1] = 0;
+}
+
+void Biquad::gainCorrectNumerator(double gain) {
+    b0 *= gain;
+    b1 *= gain;
+    b2 *= gain;
 }
--- Source/Biquad.hpp
+++ Source/Biquad.hpp
@@ -1,5 +1,5 @@
 //
-//  Biquad.hpp
+//  Biquad.h
 //  VibratoTransfer - VST3
 //
 //  Created by author on 11/18/24.
@@ -11,6 +11,7 @@
 class Biquad
 {
 public:
+    Biquad(); // empty constructor for templates
     Biquad(float _b0, float _b1, float _b2, float _a1, float _a2);
     virtual ~Biquad();
     // TODO: discuss - for simplicity it is easier to define this sample-by-sample,
@@ -19,6 +20,18 @@ public:
     virtual float processSample(float sample);
     virtual void setParams(float _b0, float _b1, float _b2, float _a1, float _a2);
     virtual void clear();
+    
+    // additions from Ruoho Ruotsi's Butterworth Filter Design codebase
+    // https://github.com/ruohoruotsi/Butterworth-Filter-Design
+    // Ruotsi's code implements a Biquad object that is largely redundant with functionality
+    // here, but has some methods that we need
+    
+    // Coefficients for a DF2T biquad section.
+    void DF2TBiquad(double B0, double B1, double B2,
+                        double A0, double A1, double A2);
+    
+    // gain correct
+    void gainCorrectNumerator(double gain);
 protected:
     float b0, b1, b2, a1, a2;
     // TODO: reimplement in canonical form for fewer floats kept around
